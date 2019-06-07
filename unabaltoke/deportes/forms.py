d4fe160983b981877ee681025d7cabf949947b3b
asiatian <asiaticous@gmail.com>
2019-06-07 00:05:34 -0400

    Cambiado la reserva

--- unabaltoke/deportes/forms.py
+++ unabaltoke/deportes/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import ReservaMusculatura
 
-class musculosForm(forms.Form):
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email')
+class MusculaturaForm(forms.Form):
+    model= ReservaMusculatura
+    CHOICES = (('1', 'Lunes'),('2', 'Martes'),('3', 'Miercoles'),('4', 'Jueves'),('5', 'Viernes'),('6', 'Sabado'))
+    dias = forms.ChoiceField(choices=CHOICES)
--- unabaltoke/deportes/models.py
+++ unabaltoke/deportes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class TallerDeportivo(models.Model):
@@ -11,9 +12,17 @@ class TallerDeportivo(models.Model):
         return "{}".format(self.nombre)
 
 class ReservaMusculatura(models.Model):
+    DAY_CHOICES = (
+        (1,'Lunes'),
+        (2,'Martes'),
+        (3,'Miercoles'),
+        (4,'Jueves'),
+        (5,'Viernes'),
+        (6,'Sabado')
+    )
     id= models.AutoField(primary_key=True)
-    nombre=models.CharField(max_length=14)
-    dia = models.IntegerField()
+    estudiante = models.ForeignKey(User, on_delete=models.CASCADE)
+    dia = models.IntegerField(choices=DAY_CHOICES)
     bloque = models.IntegerField()
     reservas=models.Manager()
 
